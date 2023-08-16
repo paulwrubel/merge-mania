da35d0c2d71f38f565dcb0243067fdc652d97100
Paul Wrubel <pwrubel7@gmail.com>
2023-08-16 02:42:57 -0500

    stuff

--- Scripts/data/block_data.gd
+++
@@ -1,9 +0,0 @@
-class_name BlockData
-extends RefCounted
-
-enum Type { POWER, WILDCARD }
-
-var type: Type
-
-func _init(_type: Type):
-	self.type = _type
--- Scripts/data/power_block_data.gd
+++
@@ -1,8 +0,0 @@
-class_name PowerBlockData
-extends BlockData
-
-var power: int
-
-func _init(_power: int):
-	super(BlockData.Type.POWER)
-	self.power = _power
